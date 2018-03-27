9373674eb2f3b02606d811bd074a1086f13f65ca
Wei <zhengwei8618@gmail.com>
2018-03-27 20:15:52 +0800

    modi by mbp

---
+++ mind_map/Machine Learning.mm
@@ -0,0 +1,254 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#0033ff" CREATED="1499667817338" ID="ID_1000208368" MODIFIED="1499738272197" STYLE="bubble" TEXT="Machine Learning">
+<edge STYLE="bezier"/>
+<node CREATED="1499685762863" ID="ID_474121630" MODIFIED="1499738272197" POSITION="right" STYLE="bubble" TEXT="&#x6700;&#x5c0f;&#x4e8c;&#x4e58;&#x6cd5;&#x4e0e;&#x6781;&#x5927;&#x4f3c;&#x7136;&#x4f30;&#x8ba1;&#x7684;&#x533a;&#x522b;">
+<node CREATED="1499685788460" ID="ID_1791489187" MODIFIED="1499738272197" STYLE="bubble" TEXT="&#x6700;&#x5c0f;&#x4e8c;&#x4e58;&#x6cd5;&#x4ece;cost/loss&#x51fd;&#x6570;&#x7684;&#x601d;&#x60f3;&#x51fa;&#x53d1;&#xff0c;&#x8ba9;&#x6a21;&#x578b;&#x7684;loss function&#x6700;&#x5c0f;"/>
+<node CREATED="1499685838895" ID="ID_495453607" MODIFIED="1499738272197" STYLE="bubble" TEXT="&#x6781;&#x5927;&#x4f3c;&#x7136;&#x4f30;&#x8ba1;&#x662f;&#x4ece;&#x6982;&#x7387;&#x7684;&#x89d2;&#x5ea6;&#x51fa;&#x53d1;&#xff0c;&#x5728;&#x4ec0;&#x4e48;&#x6837;&#x7684;&#x53c2;&#x6570;&#x4e0b;&#x6211;&#x4eec;&#x89c2;&#x6d4b;&#x5230;&#x76ee;&#x524d;&#x8fd9;&#x7ec4;&#x6570;&#x636e;&#x7684;&#x6982;&#x7387;&#x662f;&#x6700;&#x5927;&#x7684;"/>
+<node CREATED="1499686141441" ID="ID_1187512842" MODIFIED="1499738272197" STYLE="bubble" TEXT="&#x8bef;&#x5dee;&#x670d;&#x4ece;&#x9ad8;&#x65af;&#x5206;&#x5e03;&#x7684;&#x60c5;&#x51b5;&#x4e0b;&#xff0c;&#x6700;&#x5c0f;&#x4e8c;&#x4e58;&#x6cd5;=&#x6781;&#x5927;&#x4f3c;&#x7136;&#x4f30;&#x8ba1;&#x6cd5;"/>
+</node>
+<node CREATED="1499667843471" ID="ID_936885013" MODIFIED="1499738272197" POSITION="right" STYLE="bubble" TEXT="Linear Regression">
+<node CREATED="1499667888531" ID="ID_173798034" MODIFIED="1499738272197" STYLE="bubble" TEXT="&#x6700;&#x5c0f;&#x4e8c;&#x4e58;&#x6cd5;  ordinary least squares"/>
+<node CREATED="1499667954014" ID="ID_696433951" MODIFIED="1499738272197" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="../../C:/Users/Administrator/Downloads/CodeCogsEqn.png" />
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1499738190865" ID="ID_69972288" MODIFIED="1499738272197" STYLE="bubble" TEXT="&#x4e3a;&#x4ec0;&#x4e48;&#x4f7f;&#x7528;&#x8fd9;&#x4e2a;&#x4f5c;&#x4e3a;&#x635f;&#x5931;&#x51fd;&#x6570;&#xff1f;&#x53c2;&#x6570;&#x6781;&#x5927;&#x4f3c;&#x7136;&#x4f30;&#x8ba1;&#x53ef;&#x4ee5;&#x63a8;&#x5bfc;&#x8fd9;&#x4e00;&#x7ed3;&#x8bba;"/>
+<node CREATED="1499740019007" ID="ID_1441868675" MODIFIED="1499740272753">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.codecogs.com/eqnedit.php?latex=y|x;\theta&amp;space;=&amp;space;N(\mu&amp;space;,\sigma&amp;space;^{2})" target="_blank"><img src="https://latex.codecogs.com/png.latex?y|x;\theta&amp;space;=&amp;space;N(\mu&amp;space;,\sigma&amp;space;^{2})" title="y|x;\theta = N(\mu ,\sigma ^{2})" />
+</a>    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1499671964661" ID="ID_1336994208" MODIFIED="1499738272197" POSITION="right" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      LMS algorithm
+    </p>
+    <p>
+      least mean squares &#26368;&#23567;&#22343;&#26041;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1499671974682" ID="ID_718281765" MODIFIED="1499738272197" STYLE="bubble" TEXT="choose &#x3b8; to minimize J(&#x3b8;)"/>
+<node CREATED="1499672212211" ID="ID_1652706684" MODIFIED="1499738272197" STYLE="bubble" TEXT="&#x68af;&#x5ea6;&#x4e0b;&#x964d; gradient descent">
+<node CREATED="1499672494462" ID="ID_234113320" MODIFIED="1499738272197" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="../../C:/Users/Administrator/Downloads/CodeCogsEqn2.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1499673468281" ID="ID_1614167536" MODIFIED="1499738272198" STYLE="bubble" TEXT="batch gradient descent"/>
+<node CREATED="1499673480651" ID="ID_680423258" MODIFIED="1499738272198" STYLE="bubble" TEXT="stochastic gradient descent"/>
+</node>
+</node>
+<node CREATED="1499680007421" ID="ID_1003874347" MODIFIED="1499738272198" POSITION="right" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The normal equations
+    </p>
+    <p>
+      &#27861;&#26041;&#31243;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1499680047747" ID="ID_838549843" MODIFIED="1499738272198" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="../../C:/Users/Administrator/Downloads/CodeCogsEqn2%20(1).png" />
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1499680456872" ID="ID_1693428537" MODIFIED="1499738272198" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#30452;&#25509;&#27714;&#24471;
+    </p>
+    <p>
+      <img src="../../C:/Users/Administrator/Downloads/CodeCogsEqn4.png" />
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1499738390276" ID="ID_1629378171" MODIFIED="1499738403343" POSITION="right" TEXT="Logistic regression">
+<node CREATED="1499738581443" ID="ID_1813914537" MODIFIED="1499738728646">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#36923;&#36753;&#22238;&#24402;&#23545;&#24212;&#30340;&#38382;&#39064;y~(0,1)&#65292;&#25551;&#36848;&#22914;&#19979;
+    </p>
+    <p>
+      <img src="../../C:/Users/Administrator/Downloads/CodeCogsEqn5.png" />
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1499739639474" ID="ID_688622177" MODIFIED="1499739654504">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.codecogs.com/eqnedit.php?latex=\theta&amp;space;_{j}:=\theta_{j}&amp;plus;\alpha&amp;space;(y^{(i)}-h_{\theta&amp;space;}(x^{(i)}))x_{j}^{(i)}" target="_blank"><img src="https://latex.codecogs.com/png.latex?\theta&amp;space;_{j}:=\theta_{j}&amp;plus;\alpha&amp;space;(y^{(i)}-h_{\theta&amp;space;}(x^{(i)}))x_{j}^{(i)}" title="\theta _{j}:=\theta_{j}+\alpha (y^{(i)}-h_{\theta }(x^{(i)}))x_{j}^{(i)}" />
+</a>    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1499740282980" ID="ID_1339906030" MODIFIED="1499740350005">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.codecogs.com/eqnedit.php?latex=y|x;\theta&amp;space;=&amp;space;Bernoulli(\varphi&amp;space;)" target="_blank"><img src="https://latex.codecogs.com/png.latex?y|x;\theta&amp;space;=&amp;space;Bernoulli(\varphi&amp;space;)" title="y|x;\theta = Bernoulli(\varphi )" />
+</a>    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1499741761545" ID="ID_611695596" MODIFIED="1499745203030" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The exponential family
+    </p>
+    <p>
+      &#25351;&#25968;&#26063;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1499741908575" ID="ID_1055452519" MODIFIED="1499742069020">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://www.codecogs.com/eqnedit.php?latex=p(y;\eta&amp;space;)&amp;space;=&amp;space;b(y)exp(\eta^{T}T_{y}-\alpha&amp;space;(\eta))" target="_blank">&#22914;&#26524;&#19968;&#20010;&#20998;&#37096;&#21487;&#20197;&#29992;&#20197;&#19979;&#20889;&#20986;&#26469;&#65306;&#160; </a>
+    </p>
+    <p>
+      <a href="https://www.codecogs.com/eqnedit.php?latex=p(y;\eta&amp;space;)&amp;space;=&amp;space;b(y)exp(\eta^{T}T_{y}-\alpha&amp;space;(\eta))" target="_blank"><img src="https://latex.codecogs.com/png.latex?p(y;\eta&amp;space;)&amp;space;=&amp;space;b(y)exp(\eta^{T}T_{y}-\alpha&amp;space;(\eta))" title="p(y;\eta ) = b(y)exp(\eta^{T}T_{y}-\alpha (\eta))" />
+      </a>
+    </p>
+    <p>
+      eta&#20026;&#20998;&#24067;&#30340;&#33258;&#28982;&#21442;&#25968;
+    </p>
+    <p>
+      T(y)&#20026;&#20805;&#20998;&#32479;&#35745;&#37327;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1499745204702" ID="ID_1608782929" MODIFIED="1499745245719" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Generative learning algorithms
+    </p>
+    <p>
+      &#29983;&#25104;&#23398;&#20064;&#31639;&#27861;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1499745253434" ID="ID_103965073" MODIFIED="1499745798621">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#19982;&#21028;&#21035;&#23398;&#20064;&#31639;&#27861;&#30340;&#21306;&#21035;&#65306;&#23545;&#26082;&#26377;&#25968;&#25454;&#29305;&#24449;&#30340;&#20998;&#24067;&#24314;&#27169;&#65292;
+    </p>
+    <p>
+      &#26368;&#21518;&#21028;&#26029;&#23545;&#35937;&#30340;&#29305;&#24449;&#33853;&#22312;&#21738;&#20010;&#29305;&#24449;&#30340;&#27010;&#29575;&#26356;&#22810;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1499745814686" ID="ID_784995303" MODIFIED="1499745881808">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#39640;&#26031;&#21028;&#21035;&#20998;&#26512; Gaussian distriminant analysis
+    </p>
+    <ol>
+      <li>
+        &#19982;logistic&#22238;&#24402;&#27604;&#36739;&#30456;&#20284;
+      </li>
+      <li>
+        &#36866;&#29992;&#20110;&#25968;&#25454;&#29305;&#24449;&#26381;&#20174;&#39640;&#26031;&#20998;&#24067;&#30340;&#21069;&#25552;&#19979;
+      </li>
+    </ol>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1499745905328" ID="ID_1978555686" MODIFIED="1499745924747" TEXT="&#x6734;&#x7d20;&#x8d1d;&#x53f6;&#x65af; Naive Bayes">
+<node CREATED="1499746313393" ID="ID_326359509" MODIFIED="1499746438524" TEXT="&#x5f53;&#x539f;&#x751f;&#x8fde;&#x7eed;&#x503c;&#x5c5e;&#x6027;&#x4e0d;&#x5bb9;&#x6613;&#x7528;&#x591a;&#x5143;&#x6b63;&#x6001;&#x5206;&#x5e03;&#x6765;&#x5efa;&#x6a21;&#x65f6;&#xff0c; &#xa;&#x5c06;&#x7279;&#x5f81;&#x5411;&#x91cf;&#x79bb;&#x6563;&#x5316;&#x518d;&#x4f7f;&#x7528;NB&#x6765;&#x66ff;&#x4ee3;GDA&#xff0c;&#x53ef;&#x5f62;&#x6210;&#x8f83;&#x597d;&#x7684;&#x5206;&#x7c7b;&#x5668;"/>
+</node>
+</node>
+</node>
+</map>
